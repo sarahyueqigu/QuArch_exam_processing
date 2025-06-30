58ae7c66d50502b1f0ec90ef986a192614d6136d
syg <sarahdaisygu@gmail.com>
2025-06-30 12:12:26 -0400

    Resolve merge conflict

--- image_identifing.py
+++ image_identifing.py
@@ -7,52 +7,6 @@ import os
 
 client = boto3.client("bedrock-runtime", region_name="us-east-2")
 
-old_prompt = """
-Each problem in the exam may contain:
-A main context paragraph or a brief statement (e.g., “Convert the number 42 to binary”).
-One or more sub-questions, or a single standalone question.
-Sub-question-specific context that may be separate from both the main problem context and the sub-question text itself.
-Multiple question parts embedded within a single sub-question.
-Point values for problems or subproblems (including extra credit).
-Solutions, which may be typed or handwritten.
-Associated figures (tables, diagrams, circuits, or block diagrams).
-
-
-Your task:
-Match each visual element (table, diagram, circuit schematic, or block diagram) to its correct association in the exam. There are three main associations: the main problem, the sub-problem question, and the sub-problem solution. 
-
-Specifically, determine whether each visual belongs to one of the following:
-- The main problem context (labelled as problem_figure)
-- A sub-problem context (subproblem_figure)  
-- A solution (solution_figure)
-
-At times, the context or question in the main problem, sub-problem, sub-problem question, and/or sub-problem solution will include phrases (such as “The table below” or “The following diagram”) that indicate a visual image falls under that category. 
-
-Your output should be a dictionary like this:
-
-[
-  {
-    "name": "image_name.png",
-    "type": "problem_figure",
-    "problem_name": 1
-    "part": ""
-  }
-]
-
-
-If the figure is part of a subproblem, its output should represent the following, in which the "part" field is left unempty:
-[
-  {
-    "name": "image_name.png",
-    "type": "subproblem_figure",
-    "problem_name": 2
-    "part": "a"
-  }
-]
-
-Be as precise as possible in your associations. Only include the dictionary; don't include the reasoning.
-"""
-
 claude_inference_profile_arn = "arn:aws:bedrock:us-east-2:851725383897:inference-profile/us.anthropic.claude-3-7-sonnet-20250219-v1:0"
 
 def encode_image(image_path):
@@ -146,6 +100,16 @@ def process(image_file, input_dir):
         }
     ]
 
+    If the figure is part of a subproblem, its output should represent the following, in which the "part" field is left unempty:
+    [
+        {
+            "name": "image_name.png",
+            "type": "solution",
+            "part": "a",
+            "problem_name": """ + question_name + """
+            
+        }
+    ]
 
     If the figure is part of a subproblem, its output should represent the following, in which the "part" field is left unempty:
     [
--- main.py
+++ main.py
@@ -6,47 +6,35 @@ import image_identifing
 import problem_page_extraction
 
 if __name__ == "__main__":
-    problem_page_extraction.split_exams("OnurETHZ_exams")
+    # problem_page_extraction.execute_single("data", "ddca-s23-en-sol.pdf")
+    problem_page_extraction.execute("data")
 
-    parent_folder = "OnurETHZ_exams_parsed1"
+    parent_folder = "extracted_problems"
     print(os.listdir(parent_folder))
     # os.makedirs("OnurETHZ_exams", exist_ok=True)
     input_dir = parent_folder
 
-    # for folder in os.listdir(parent_folder):
-    #     input_dir =  parent_folder + "/" + folder
-    #     print("input_dir:", input_dir)
+    for folder in os.listdir(parent_folder):
+        input_dir =  parent_folder + "/" + folder
+        print("input_dir:", input_dir)
 
         # for problem_folder in os.listdir(input_dir):
 
         #     input_dir = parent_folder + "/" + folder + "/" + problem_folder
-        #     # Extract all the images in each file at once
-        #     # image_extraction.process(input_dir)
 
-        # # Ensure this is not hte .DS_Store file
-        # if folder != ".DS_Store":
+        # Ensure this is not hte .DS_Store file
+        if folder != ".DS_Store":
 
-    # Parse through each file in the subfolder
-    for filename in os.listdir(input_dir): # TODO: may cause some delays when .json files are added
-        # Only process PDFs
-        if filename.lower().endswith(".pdf"):             
-            # Output path
-            output_dir = input_dir + "/" + filename[:-4]
-            os.makedirs(output_dir, exist_ok=True)
+            # Parse through each file in the subfolder
+            for filename in os.listdir(input_dir): # TODO: may cause some delays when .json files are added
+                # Only process PDFs
+                if filename.lower().endswith(".pdf"): 
+                    # # Output path
+                    # output_dir = input_dir + "/" + filename[:-4]
+                    # os.makedirs(output_dir, exist_ok=True)
 
-            print("output_dir: ", output_dir)
+                    # print("output_dir: ", output_dir)
 
-            text_extraction.process(filename, input_dir + "/" + filename, input_dir)
-            image_extraction_singlepdf.process(filename, input_dir + "/" + filename, input_dir + "/" + filename[:-4])
-            image_identifing.process(filename, input_dir + "/" + filename[:-4])
-        
-    
-        
-        # # Create the image (use scandir for more speed)
-        # with os.scandir(folder) as it:
-        #     for entry in it:
-        #         if entry.is_dir():
-                    
-        
-        
-        
+                    text_extraction.process(filename, input_dir + "/" + filename, input_dir)
+                    image_extraction_singlepdf.process(filename, input_dir + "/" + filename, input_dir + "/" + filename[:-4])
+                    image_identifing.process(filename, input_dir + "/" + filename[:-4])
