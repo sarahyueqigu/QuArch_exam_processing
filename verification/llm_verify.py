973144760b8a470db68355d0167f60aa150a9d5f
a-slices <aliceguo05@gmail.com>
2025-07-07 13:54:10 -0400

    starting verification

---
+++ verification/llm_verify.py
@@ -0,0 +1,6 @@
+##############
+#Using the AWS Bedrock API, we call various other LLMs to verify the output problem json files
+##############
+
+#input: the .json file of the main pipeline output, the original exam pdf file, and the problem image folder corresponding to that exam
+
