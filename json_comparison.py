7244232b0c39fd85d322ecadf720cc1e81aaa525
syg <sarahdaisygu@gmail.com>
2025-07-14 15:47:51 -0400

    updated verificatin_checking.py to also measure accuracy of non-image questions

--- json_comparison.py
+++ json_comparison.py
@@ -10,81 +10,72 @@ client = boto3.client("bedrock-runtime", region_name="us-east-2")
 
 arn = config.claude_37
 
+
 # e.g. 
 def process(exam):
 
 # Attatched are (1) an exam pdf, (2) images extracted for that particular question, and (3)
-
-    prompt = """You are are a JSON accuracy checker. Attatched are multiple versions of the same 
-    content extracted in the format of a dictionary/json from different LLM models. Your task is to 
-    compare the content of each dictionary/JSON field by field. Identify discrepancies in extracted 
-    values, missing fields, or format differences. 
-
-    Your output should be a dictionary like this, and ONLY this:
-    [
-        {
-            "question_id": <Insert the "question_id" from the txt file>, 
-            "correctly_parsed": <true or false>
-        },
-        {
-            "question_id": <Insert the "question_id" from the txt file>, 
-            "correctly_parsed": <true or false>
-        },
-        {
-            "question_id": <Insert the "question_id" from the txt file>, 
-            "correctly_parsed": <true or false>
-        },
-        {
-            ... repeat for as many problems there are in each content extraction JSON
-        },
-    ]
-
-    If there are any discrepancies between the content (e.g. the material rather than the point values) 
-    of each version of dictionaries/JSONs, the field "correctly_parsed" should be set to false, like 
-    this:
-    {
-        "question_id": "digitaltechnik-s21-en-sol/Problem_10/a", 
-        "correctly_parsed": false
-    }
-     
-     Here is the format of exam content extraction:
-        “question_id”: “exam_name/Problem_number/subproblem_letter” (e.g. “a”, “b”, etc.),
-        “context”: “<Insert any introductory paragraph or description exactly as it appears. If there is no context, don’t include this header>“,
-        “context_figures”: “file_directory_path/exam_name/img_number”,
-        “question”: “<Insert the full question of the problem, exactly as it appears in the original>“,
-        “solution”: “<Insert the full solution of the problem, exactly as shown in the original>“,
-        “solution_figures”: “file_directory_path/exam_name/img_number”,
-        “correctly_parsed”: “null” (indicating if the problem was parsed correctly).
-        
-    Attatched below is each version of the actual extracted content from the exam
-
-    """
     
-    input_dir = "out_model_comparison/" + exam[:-5]
-    # Iterate through each output from each llm model
-    for llm_model in os.listdir(input_dir):
-        json_path = os.path.join(input_dir, llm_model)
+    input_dir = "out_model_comparison/" + exam
 
-        # Read JSON file as it is
-        with open(json_path, 'r', encoding='utf-8') as file:
-            json_problems = json.load(file)
+    # Open one JSON file to iterate thru the questions
+    json_path = os.path.join(input_dir, "claude_37.json")
+    with open(json_path, 'r', encoding='utf-8') as file:
+        placeholder_problems = json.load(file)
+
+    # Iterate through each question in each JSON
+    for problem in placeholder_problems: 
         
-        # Read the problem as a string to feed into the AI
-        problem_str = json.dumps(json_problems, indent=2)
+        # If errors occur because the question_id doesn't work, use:
+        # for index in enumerate(json_content):
 
-        prompt += """LLM model: """ + llm_model + "\n Extracted exam content:\n" + problem_str 
+        # Iterate through each output from each llm model
+        for llm_model in os.listdir(input_dir):
+            json_path = os.path.join(input_dir, llm_model)
+
+            # Read JSON file as it is
+            with open(json_path, 'r', encoding='utf-8') as file:
+                problems_version = json.load(file)
+            
+            problem_content = next((item for item in problems_version 
+                                    if problems_version["question_id"] == problem["question_id"]), None)
+            
+
+            print("problem_content:", problem_content)
+
+
+
+
+            json_path = os.path.join(input_dir, llm_model)
+
+            # Read JSON file as it is
+            with open(json_path, 'r', encoding='utf-8') as file:
+                json_problems = json.load(file)
+            
+            # Read the problem as a string to feed into the AI
+            problem_str = json.dumps(json_problems, indent=2)
+
+            prompt += """LLM model: """ + llm_model + "\n Extracted exam content:\n" + problem_str 
+    
     
     result = claud_37_processing(prompt)
-
-    json_result = helper.strip_json_code_block(result)
-
-    # with open(preprocessed_output + "/" + problem + ".json", 'w') as file:
-    #     json.dump(problem_dict, file, indent=4)
-
-    print(prompt)
-
     print("\n \nRESULT:\n")
-    print(result)
+
+    json_str = helper.strip_json_code_block(result)
+    print(json_str)
+    json_result = json.loads(json_str)
+
+    os.makedirs("llm_verification_v2", exist_ok=True)
+    output_path = os.path.join("llm_verification_v2", exam + ".json")
+
+    with open(output_path, 'w') as file:
+        json.dump(json_result, file, indent=4)
+    
+    print("SAVED TO: ", output_path)
+
+    # print(prompt)
+
+   
 
     
 
@@ -106,7 +97,7 @@ def claud_37_processing(prompt):
         response = client.converse(
             modelId=arn,
             messages=conversation,
-            inferenceConfig={"maxTokens": 8000, "temperature": 0},
+            inferenceConfig={"maxTokens": 8120, "temperature": 0.1},
         )
 
         # Extract and print the response text.
@@ -124,7 +115,7 @@ def claud_37_processing(prompt):
 # TODO: turn this into a dictionary and loop through them later
    
 if __name__ == "__main__":
-    exam = "740_f13_midterm2_solutions.json"
+    exam = "740_f13_midterm2_solutions"
 
     process(exam)
 
--- verification_checking.py
+++ verification_checking.py
@@ -2,8 +2,12 @@ import json
 import os
 import csv
 
-fieldnames = ["exam", "api", "total", "concurred_trues", "concurred_falses", "checked_trues", 
-              "api_trues", "api_accuracy"]
+fieldnames = ["exam", "api", "total", "concurred_trues", "concurred_falses", "false_trues", "true_falses",
+              "verified_trues", "api_trues", "total_questions_no_images", "concurred_trues_no_images", 
+              "concurred_falses_no_images", "api_accuracy", "llm_accuracy", "llm_accuracy_no_images"]
+
+# false_trues is where the api thinks it's true when it's actually false
+# true_falses is where the api thinks it's false when it's actually true 
 
 # input_dir is from verification_tests/exam_name
 def process(input_dir, exam):
@@ -15,41 +19,63 @@ def process(input_dir, exam):
 
         row = {
             "exam": exam,
-            "api": api_test[:-5],
+            "api": "ALL (verification pipeline version 2)",
             "total": 0.0,
             "concurred_trues": 0.0,
             "concurred_falses": 0.0, 
-            "checked_trues": 0.0, 
+            "false_trues": 0.0,
+            "true_falses": 0.0,
+            "verified_trues": 0.0, 
             "api_trues": 0.0, 
-            "api_accuracy": 0.0
+            "total_questions_no_images": 0.0,
+            "concurred_trues_no_images": 0.0,
+            "concurred_falses_no_images": 0.0,
+            "llm_accuracy": 0.0,
+            "llm_accuracy_no_images": 0.0
         }
         
         json_path = os.path.join(input_dir, api_test)
         with open(json_path, "r") as file:
             verification_test_json = json.load(file)
         
-        # Open the manuall ychecked results
+        # Open the hand_verifiedly checked results
         checked_path = os.path.join("checked", "[CHECKED]" + exam + ".json")
         with open(checked_path, "r") as file:
-            manually_checked_json = json.load(file)
+            hand_verifiedly_checked_json = json.load(file)
         
-        for verify, manual in zip(verification_test_json, manually_checked_json):
+        for llm_verified, hand_verified in zip(verification_test_json, hand_verifiedly_checked_json):
+            print("Processing problem:", llm_verified["question_id"])
             row["total"] += 1
-            # print("verify[correctly_parsed]: ", verify["correctly_parsed"])
-            # print("manual[correctly_parsed]: ", manual["correctly_parsed"])
+            # print("llm_verified[correctly_parsed]: ", llm_verified["correctly_parsed"])
+            # print("hand_verified[correctly_parsed]: ", hand_verified["correctly_parsed"])
 
-            if verify["correctly_parsed"] == True and manual["correctly_parsed"] == True:
+            # Check if the problem has images
+            has_images = True
+            if not hand_verified["context_figures"] and not hand_verified["solution_figures"]:
+                has_images = False
+                row["total_questions_no_images"] += 1
+
+            if llm_verified["correctly_parsed"] == True and hand_verified["correctly_parsed"] == True:
                 row["concurred_trues"] += 1
-            elif verify["correctly_parsed"] == False and manual["correctly_parsed"] == False:
+                if not has_images:
+                    row["concurred_trues_no_images"] += 1
+            elif llm_verified["correctly_parsed"] == False and hand_verified["correctly_parsed"] == False:
                 row["concurred_falses"] += 1
+                if not has_images:
+                    row["concurred_falses_no_images"] += 1
+            elif llm_verified["correctly_parsed"] == True and hand_verified["correctly_parsed"] == False:
+                row["true_falses"] += 1
+            else:
+                row["false_trues"] += 1
 
-            if verify["correctly_parsed"] == True:
+            if llm_verified["correctly_parsed"] == True:
                 row["api_trues"] += 1
-            if manual["correctly_parsed"] == True:
-                row["checked_trues"] += 1
-                print("went here")
+            if hand_verified["correctly_parsed"] == True:
+                row["verified_trues"] += 1
         
-        row["api_accuracy"] = (row["concurred_trues"] + row["concurred_falses"]) / row["total"]
+        row["llm_accuracy"] = (row["concurred_trues"] + row["concurred_falses"]) / row["total"]
+        row["llm_accuracy_no_images"] = (row["concurred_falses_no_images"] + row["concurred_trues_no_images"]) / row["total"]
+        print(row)
 
         # Open the CSV in append mode
         with open('verification_checking.csv', 'a', newline='') as file:
@@ -62,8 +88,11 @@ def process(input_dir, exam):
 
 if __name__ == "__main__":
     exam = "740_f13_midterm2_solutions"
-    parent_dir = os.path.join("verification_tests", exam)
-    process(parent_dir, exam)
+    # parent_dir = os.path.join("llm_verification_v2", exam)
+    process("llm_verification_v2", exam)
+
+    # with open("verification_checking.csv", "w", newline="") as file:
+    #     writer = csv.DictWriter(file, fieldnames=fieldnames)
 
     # for exam in os.listdir(parent_dir):
         
